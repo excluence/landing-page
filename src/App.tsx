12ed9b8ff6324adc8a843ecee92ae0b5ceafbd00
Piyush Jaiswal <iampiyushjaiswal103@gmail.com>
2023-05-12 03:52:42 +0530

    Added react-spring pkg, images and svg

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,18 @@
-import { useState } from 'react'
-import viteLogo from '/vite.svg'
 import './App.css'
-import { Typography, Button } from '@mui/material'
+import {Parallax, ParallaxLayer} from '@react-spring/parallax';
+
+// Page Sections
+import HomeSection from './HomeSection'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <Button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </Button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-        <Typography variant="h1" >The Pajama Collection</Typography>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Parallax pages={1} >
+          <ParallaxLayer offset={0}>
+              <HomeSection />
+          </ParallaxLayer>
+      </Parallax>
     </>
   )
 }
